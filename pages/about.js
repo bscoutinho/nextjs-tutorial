097bd2cdd53ced3ae6cc44521f1a434628a02e88
unknown <bscoutinho2012@gmail.com>
2021-02-08 10:56:34 +0000

    first commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <>
+        <Head>
+            <title>Ninja List | About</title>
+            <meta name='keywords' content='ninjas' />
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem 1</p>
+            <p>Lorem 2</p>
+        </div>
+        </>
+    )
+}
+
+export default about
